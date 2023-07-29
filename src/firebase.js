d6383c8a514843b4d12fb6af3d2c9cb1095af910
Meghna Agrawal <meghnaagrawal10@gmail.com>
2023-07-29 18:21:21 +0530

    Header: logo and search bar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBduarmxlqOL8VkoIIjkbm4l4RniTwnD9M",
+  authDomain: "clone-ab316.firebaseapp.com",
+  projectId: "clone-ab316",
+  storageBucket: "clone-ab316.appspot.com",
+  messagingSenderId: "909414777833",
+  appId: "1:909414777833:web:25f424205ffa35fafb8178",
+  measurementId: "G-LXLNQBSCY7"
+};
